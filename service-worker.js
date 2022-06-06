de9d19f63e202ac91b0416982565236153928404
Lash Blast <evelyn.aguirre.9895@gmail.com>
2022-06-06 10:05:08 -0500

    add offline functionality

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,3 @@
-const APP_PREFIX = "FoodFest-";
-const VERSION = "version_01";
-const CACHE_NAME = APP_PREFIX + VERSION;
-
 const FILES_TO_CACHE = [
 	"./index.html",
 	"./events.html",
@@ -15,34 +11,9 @@ const FILES_TO_CACHE = [
 	"./dist/tickets.bundle.js",
 	"./dist/schedule.bundle.js",
 ];
-
-self.addEventListener("install", function (e) {
-	e.waitUntil(
-		caches.open(CACHE_NAME).then(function (cache) {
-			console.log("installing cache : " + CACHE_NAME);
-			return cache.addAll(FILES_TO_CACHE);
-		})
-	);
-});
-
-self.addEventListener("activate", function (e) {
-	e.waitUntil(
-		caches.keys().then(function (keyList) {
-			let cacheKeeplist = keyList.filter(function (key) {
-				return key.indexOf(APP_PREFIX);
-			});
-			cacheKeeplist.push(CACHE_NAME);
-			return Promise.all(
-				keyList.map(function (key, i) {
-					if (cacheKeeplist.indexOf(key) === -1) {
-						console.log("deleting cache : " + keyList[i]);
-						return caches.delete(keyList[i]);
-					}
-				})
-			);
-		})
-	);
-});
+const APP_PREFIX = "FoodFest-";
+const VERSION = "version_01";
+const CACHE_NAME = APP_PREFIX + VERSION;
 
 self.addEventListener("fetch", function (e) {
 	console.log("fetch request : " + e.request.url);
@@ -60,6 +31,35 @@ self.addEventListener("fetch", function (e) {
 
 			// You can omit if/else for console.log & put one line below like this too.
 			// return request || fetch(e.request)
+		})
+	);
+});
+
+self.addEventListener("install", function (e) {
+	e.waitUntil(
+		caches.open(CACHE_NAME).then(function (cache) {
+			console.log("installing cache : " + CACHE_NAME);
+			return cache.addAll(FILES_TO_CACHE);
+		})
+	);
+});
+
+self.addEventListener("activate", function (e) {
+	e.waitUntil(
+		caches.keys().then(function (keyList) {
+			let cacheKeeplist = keyList.filter(function (key) {
+				return key.indexOf(APP_PREFIX);
+			});
+			cacheKeeplist.push(CACHE_NAME);
+
+			return Promise.all(
+				keyList.map(function (key, i) {
+					if (cacheKeeplist.indexOf(key) === -1) {
+						console.log("deleting cache : " + keyList[i]);
+						return caches.delete(keyList[i]);
+					}
+				})
+			);
 		})
 	);
 });
