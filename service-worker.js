8debf9299a70fc23948997a4416a3c0c8e2556ad
Lash Blast <evelyn.aguirre.9895@gmail.com>
2022-06-06 11:09:58 -0500

    create manifest

--- service-worker.js
+++ service-worker.js
@@ -30,7 +30,7 @@ self.addEventListener("fetch", function (e) {
 			}
 
 			// You can omit if/else for console.log & put one line below like this too.
-			// return request || fetch(e.request)
+			// return request || fetch(e.request);
 		})
 	);
 });
